3c527b9d938eb78158979d389142aa6a40e59ef7
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-19 20:18:03 +0200

    R&M 2

--- src/components/Episode/Episodes.js
+++ src/components/Episode/Episodes.js
@@ -4,14 +4,32 @@ import Episode from "./Episode";
 
 const Episodes = () => {
     const [episodes, setEpisodes] = useState([])
+    const [numberPage, setnumberPage] = useState()
+    const [page,setPage] = useState(1)
 
     useEffect(()=>{
-    episodeServ.getEpisode().then(value => setEpisodes([...value.results]))
-    },[])
+    episodeServ.getEpisode(page).then(value => {
+        setEpisodes([...value.results])
+            setnumberPage(value.info.page)
+    })
+    },[page])
+
+    const forward = () => {
+            setPage(page +1)
+    }
+    const back = () => {
+            setPage(page -1)
+    }
 console.log(episodes)
     return (
         <div>
+
+            <div> Rick and Morty episodes</div>
+            <button onClick={forward}>Forward</button>
+            <button onClick={back}>Back</button>
             {episodes.map(episode => <Episode key={episode.id} episodeInfo={episode}/>)}
+
+
         </div>
     );
 };
