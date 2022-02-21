f37ef8c05ef16b9373db2806c9e2ead321318a78
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-21 18:17:29 +0200

    train

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 function App() {
+
+  // let [info, setInfo] = useState({username :'',pass:''})
+
+  // const inform = (e) => {
+  //   setInfo({...info,[e.target.name]: e.target.value})
+  // }
+
+// const submit = (e) => {
+//   e.preventDefault()
+// console.log(info)
+// }
+
+const {register, handleSubmit} =  useForm()
+
+    const onSub = (data) => {
+      console.log(data)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<form onSubmit={submit}>*/}
+        {/*<input type="text" name={'username'} value={info.username} onChange={inform}/>*/}
+        {/*<input type="text" name={'pass'} value={info.pass} onChange={inform} />*/}
+        {/*<button>sdfsd</button>*/}
+      {/*</form>*/}
+      <form  onSubmit={handleSubmit(onSub)}>
+          <input type="text" {...register('name')} defaultValue={''}/>
+          <input type="text" {...register('age')} defaultValue={''}/>
+          <button>asdasd</button>
+      </form>
     </div>
   );
 }
